d9d6dcc6571fd84f502ead4657e639726b02d2dd
Tigran Bayburtsyan <tigran.bayburtsyan@gmail.com>
2015-07-13 23:04:45 +0400

    adding ngTagInput

--- front/app/app.js
+++ front/app/app.js
@@ -1,3 +1,11 @@
-/**
- * Created by tigran on 7/13/15.
- */
+var app = angular.module("social-news", ['ngTagsInput']);
+
+app.controller("SearchCtrl", function ($scope, $http) {
+    $scope.tags = [];
+    $scope.loadTags = function(query) {
+        return $http.get('cities.json');
+    };
+    $scope.added = function(){
+        console.log($scope.tags);
+    };
+});
