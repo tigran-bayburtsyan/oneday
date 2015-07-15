bab2d651dcbce958992e20f0a7c1a57498ad89d6
Tigran Bayburtsyan <tigran.bayburtsyan@gmail.com>
2015-07-15 22:31:20 +0400

    adding search dates

--- front/app/app.js
+++ front/app/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("social-news", ['ngTagsInput']);
+var app = angular.module("social-news", ['ngTagsInput', 'ui.bootstrap']);
 
 app.controller("SearchCtrl", function ($scope, $http) {
     $scope.tags = [];
@@ -8,4 +8,30 @@ app.controller("SearchCtrl", function ($scope, $http) {
     $scope.added = function(){
         console.log($scope.tags);
     };
+
+    $scope.maxDate = new Date();
+    $scope.search_date = {
+        from: {
+            opened: false,
+            date: new Date()
+        },
+        to: {
+            opened: false,
+            date: new Date()
+        }
+    };
+
+    $scope.open = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.search_date.from.opened = true;
+        $scope.search_date.to.opened = true;
+    };
+});
+
+$(function () {
+    $(window).resize(function () {
+        $('.container').css("marginTop", ($(window).height() - $('.container').height())/2 + "px");
+    }).resize();
 });
