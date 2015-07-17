a0a0a857e9bef613d70433d2d103585044e0b5a4
Tigran Bayburtsyan <tigran.bayburtsyan@gmail.com>
2015-07-17 21:07:04 +0400

    adding posts functionality

--- front/app/app.js
+++ front/app/app.js
@@ -6,7 +6,28 @@ app.controller("SearchCtrl", function ($scope, $http) {
         return $http.get('cities.json');
     };
     $scope.added = function(){
-        console.log($scope.tags);
+        $scope.posts = [
+            {
+                user: {
+                    name: "Morrisey Video",
+                    url: "https://twitter.com/SethMorrisey",
+                    photo: "https://pbs.twimg.com/profile_images/534943592217726977/a90MLfyk_400x400.jpeg"
+                },
+                content: "Post Content",
+                date: "12:52 PM - 22 Jun 2015",
+                url: "https://twitter.com/SethMorrisey/status/613072157757706240"
+            },
+            {
+                user: {
+                    name: "Morrisey Video",
+                    url: "https://twitter.com/SethMorrisey",
+                    photo: "https://pbs.twimg.com/profile_images/534943592217726977/a90MLfyk_400x400.jpeg"
+                },
+                content: "Post Content",
+                date: "12:52 PM - 22 Jun 2015",
+                url: "https://twitter.com/SethMorrisey/status/613072157757706240"
+            }
+        ];
     };
 
     $scope.maxDate = new Date();
@@ -28,10 +49,21 @@ app.controller("SearchCtrl", function ($scope, $http) {
         $scope.search_date.from.opened = true;
         $scope.search_date.to.opened = true;
     };
+
+    $scope.posts = [];
+
+    $scope.$watch("posts", function () {
+        if($scope.posts.length > 0)
+        {
+            $('.search_box_wrapper').animate({
+                "marginTop": "15px"
+            }, 500);
+        }
+    });
 });
 
 $(function () {
-    var page_container =  $('.container');
+    var page_container =  $('.search_box_wrapper');
     $(window).resize(function () {
         page_container.css("marginTop", ($(window).height() - page_container.height())/2 + "px");
     }).resize();
